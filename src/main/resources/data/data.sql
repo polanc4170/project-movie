2e4f65fe24569fdc0156cf3f2a4127322ce86d0e
Uros Polanc <up4472@student.uni-lj.si>
2024-02-17 15:00:27 +0000

    Commit #05 - Added SQL Init

---
+++ src/main/resources/data/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO movies (imdb_id, title, year, description) VALUES
+(1, 'Star Wars: Episode I – The Phantom Menace'     , 1999, 'Two Jedi escape a hostile blockade to find allies and come across a young boy who may bring balance to the Force, but the long dormant Sith resurface to claim their original glory.'),
+(2, 'Star Wars: Episode II – Attack of the Clones'  , 2002, 'Ten years after initially meeting, Anakin Skywalker shares a forbidden romance with Padmé Amidala, while Obi-Wan Kenobi discovers a secret clone army crafted for the Jedi.'),
+(3, 'Star Wars: Episode III - Revenge of the Sith'  , 2005, 'Three years into the Clone Wars, Obi-Wan pursues a new threat, while Anakin is lured by Chancellor Palpatine into a sinister plot to rule the galaxy.'),
+(4, 'Star Wars: Episode IV - A New Hope'            , 1977, 'Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire''s world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.'),
+(5, 'Star Wars: Episode V - The Empire Strikes Back', 1980, 'After the Rebels are overpowered by the Empire, Luke Skywalker begins his Jedi training with Yoda, while his friends are pursued across the galaxy by Darth Vader and bounty hunter Boba Fett.'),
+(6, 'Star Wars: Episode VI - Return of the Jedi'    , 1983, 'After rescuing Han Solo from Jabba the Hutt, the Rebels attempt to destroy the second Death Star, while Luke struggles to help Darth Vader back from the dark side.');
---
+++ src/main/resources/data/schema.sql
@@ -0,0 +1,49 @@
+--Delete constraint from movies_images:
+ALTER TABLE IF EXISTS movies_images
+DROP CONSTRAINT IF EXISTS FKt648xh7iv5r8khv4u4kxbqqvv;
+
+--Delete constraint from movies_images:
+ALTER TABLE IF EXISTS movies_images
+DROP CONSTRAINT IF EXISTS FKbxrlom4skf0pso8w8i2ilx7n6;
+
+--Delete tables if they exist:
+DROP TABLE IF EXISTS images        CASCADE;
+DROP TABLE IF EXISTS movies        CASCADE;
+DROP TABLE IF EXISTS movies_images CASCADE;
+
+--Create images table:
+CREATE TABLE images (
+	id    BIGSERIAL   NOT NULL,
+	uuid  VARCHAR(64) NOT NULL UNIQUE,
+	bytes OID,
+	PRIMARY KEY (id)
+);
+
+--Create movies table:
+CREATE TABLE movies (
+	id          BIGSERIAL    NOT NULL,
+	imdb_id     BIGINT       NOT NULL UNIQUE,
+	title       VARCHAR(128) NOT NULL,
+	year        INTEGER      NOT NULL,
+	description VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+--Create one-to-many table:
+CREATE TABLE movies_images (
+	images_id BIGINT NOT NULL UNIQUE,
+	movie_id  BIGINT NOT NULL,
+	PRIMARY KEY (images_id, movie_id)
+);
+
+--Add constraint:
+ALTER TABLE IF EXISTS movies_images
+ADD CONSTRAINT FKt648xh7iv5r8khv4u4kxbqqvv
+FOREIGN KEY (images_id)
+REFERENCES images;
+
+--Add constraint:
+ALTER TABLE IF EXISTS movies_images
+ADD CONSTRAINT FKbxrlom4skf0pso8w8i2ilx7n6
+FOREIGN KEY (movie_id)
+REFERENCES movies;
